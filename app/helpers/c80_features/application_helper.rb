644c79fdbfe82e41091d84cf970576c264ea13cc
C80609A <c080609a@gmail.com>
2016-06-08 12:20:11 +0300

    master: some changes

--- app/helpers/c80_features/application_helper.rb
+++ app/helpers/c80_features/application_helper.rb
@@ -1,6 +1,14 @@
 module C80Features
   module ApplicationHelper
 
+    def render_features_block_with_images2
+      para_all = C80Features::Para.all
+      render :partial => 'c80_features/features_block_with_images2',
+             :locals => {
+                 :para_all => para_all
+             }
+    end
+
     def render_features_block_with_images
       para_all = C80Features::Para.all
       render :partial => 'c80_features/features_block_with_images',
--- lib/c80_features/version.rb
+++ lib/c80_features/version.rb
@@ -1,3 +1,3 @@
 module C80Features
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
